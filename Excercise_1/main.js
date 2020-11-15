42d7d718cf0776dfea1bd68b3f99c42ec9605092
unknown <testingame47@gmail.com>
2020-11-15 23:55:54 +0700

    up unit-14

--- Excercise_1/main.js
+++
@@ -1,6 +0,0 @@
-const getSum = () => {
-  const firstNum = parseInt(document.getElementById('firstNumber').value)
-  const secondNum = parseInt(document.getElementById('secondNumber').value)
-  const sum = firstNum + secondNum
-  document.getElementById('result').innerHTML = sum
-}
--- Excercise_2/main.js
+++
@@ -1,4 +0,0 @@
-const changeColor = event => {
-  const presentColor = event.target
-  return presentColor.style.background == 'red' ? presentColor.style.background = 'green' : presentColor.style.background = 'red'
-}
